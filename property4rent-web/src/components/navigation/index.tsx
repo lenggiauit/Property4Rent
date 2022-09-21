862b331505de89f60c18ff22c1120de560a278e2
Lenggiauit <lenggiauit@gmail.com>
2022-09-20 23:05:33 -0400

    adding menu

--- property4rent-web/src/components/navigation/index.tsx
+++ property4rent-web/src/components/navigation/index.tsx
@@ -55,59 +55,66 @@ const Navigation: React.FC<Props> = ({ isPublic, navCssClass, currentUser }) => 
                             <AnimationLogo width={45} height={45} />
                         </a>
                     </div>
-                    <section className="navbar-mobile">
+                    <section className="navbar-mobile ">
                         <span className="navbar-divider d-mobile-none"></span>
+                       
                         <ul className="nav nav-navbar nav-text-normal mr-auto">
-                            <li className="nav-item">
+                            {/* <li>
                                 <a className={`nav-link ${(location.pathname == '/') ? "active" : ""}`} href="/"><Translation tid="nav_home" /></a>
-                            </li>
-                            {currentUser != null && <>
-                                <li className="nav-item">
-                                    <a className={`nav-link ${(location.pathname.indexOf('resume') != -1) ? "active" : ""}`} href="#"><Translation tid="nav_resume" /><span className="arrow"></span></a>
-                                    <nav className="nav">
-                                        <a className="nav-link" href="/yourresume"><Translation tid="nav_yourResume" /></a>
-                                        <a className="nav-link" href="/createresume"><Translation tid="nav_createResume" /></a>
-                                    </nav>
-                                </li>
-                            </>
-                            }
+                            </li>  */}
+                            <li>
+                                <a className={`nav-link ${(location.pathname == '/findahouse') ? "active" : ""}`} href="/"><Translation tid="nav_findahouse" /></a>
+                            </li> 
+
+                        </ul>
+                        <div className="justify-content-end">
+                           <ul className="nav nav-navbar nav-text-normal mr-auto ">
+                            
                             {currentUser != null
                                 && (hasPermissions([PermissionKeys.CreateTemplateType, PermissionKeys.GetTemplate, PermissionKeys.GetTemplateType, PermissionKeys.UploadTemplate]))
                                 && <>
                                     <li className="nav-item">
                                         <a className={`nav-link ${(location.pathname.indexOf('admin') != -1) ? "active" : ""}`} href="#"><Translation tid="nav_admin" /><span className="arrow"></span></a>
                                         <nav className="nav">
-                                            <a className="nav-link" href="/admin/templatetype">Template Type</a>
-                                            <a className="nav-link" href="/admin/template">Template</a>
-                                            <a className="nav-link" href="/admin/resumes">Resumes</a>
+                                            <a className="nav-link" href="/admin/propertytype">Property Type</a>
+                                            <a className="nav-link" href="/admin/propertylist">Property List</a>
+                                            <a className="nav-link" href="/admin/userlist">User List</a>
                                         </nav>
                                     </li>
                                 </>
                             }
-                            {currentUser != null && <>
+ 
+                           {currentUser != null && <>
                                 <li className="nav-item">
-                                    <a className={`nav-link ${(location.pathname.indexOf('messages') != -1) ? "active" : ""}`} href="/messages"><Translation tid="nav_messages" />
-                                    {messageCount == 0 ? "" : <> ({ messageCount})</>  }</a>
+                                    <a className={`nav-link ${(location.pathname.indexOf('notification') != -1) ? "active" : ""}`} href="/notification">
+                                        <i className="bi bi-bell" style={{fontSize: 24}}></i>
+                                    </a>
                                 </li>
+                                <li className="nav-item">  
+                                        <a className={`nav-link ${(location.pathname.indexOf('messages') != -1) ? "active" : ""}`} href="/messages">
+                                            
+                                        <i className="bi bi-chat-text" style={{fontSize: 24}}></i>
+
+                                        {messageCount == 0 ? "" : <> ({ messageCount})</>  }</a>
+                                </li> 
                                 <li className="nav-item">
-                                    <a className={`nav-link ${(location.pathname.indexOf('profile') != -1) ? "active" : ""}`} href="/profile"><Translation tid="nav_profile" /></a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#" onClick={() => logout()}><Translation tid="nav_logout" /></a>
-                                </li>
+                                    <a className={`nav-link ${(location.pathname.indexOf('user') != -1) ? "active" : ""}`} href="#"> 
+                                    <i className="bi bi-person-circle" style={{fontSize: 24}}></i></a>
+                                    <nav className="nav">
+                                        <a className="nav-link" href="/profile"><Translation tid="nav_profile" /> </a>
+                                        <a className="nav-link" href="/propertylist">Your Property</a>
+                                        <a className="nav-link" href="#" onClick={() => logout()}><Translation tid="nav_logout" /></a>
+                                    </nav>
+                                </li> 
                             </>
                             }
-                            
-                        </ul>
-                        <div>
-                           
-                            <a className="btn btn-sm btn-success" href="#">Donate</a>
-                            <span className="navbar-divider d-mobile-none"></span>
+                           </ul> 
+                           </div>
                             {isPublic && currentUser == null && <> 
                             <a className="btn btn-sm btn-success" href="/login"><Translation tid="nav_login" /></a> 
                             <span className="navbar-divider d-mobile-none"></span>
                             </>}
-                        </div>
+                         
                         <LanguageSelector />
                     </section>
 
